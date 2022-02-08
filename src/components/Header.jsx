90e9e5e53cf898f023f9d3fc8b4c03bfada8bd43
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-02-08 01:16:29 +0100

    added Header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props =>{
+    return <header>
+        <span className="populares">Películas Populares</span>
+        <span className="upcoming">Próximas películas</span>
+        <span className="populares">Películas más votadas</span>
+    </header>
+}
+export default Header;
