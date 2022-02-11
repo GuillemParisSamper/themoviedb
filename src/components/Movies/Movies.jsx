14af414b80f4152faea2d2519e7358dd2054fbf4
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-02-11 01:19:26 +0100

    added axios to get movies list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Movies.scss';
 
 const Movies = (props) => {
     const {movieType} = useParams();
-    //console.log("test")
-    //console.log(`/${movieType}`)
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        axios.get(`https://api.themoviedb.org/3/movie/${movieType}?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES`)
+        .then(res => setMovies(res.data.results))
+        .catch(console.error)
+    },[])
+ 
     return <div className="movies">
         Tipo de películas: {movieType}
+        {movies?.map(movie=>movie.title)}
     </div>
 }
 
