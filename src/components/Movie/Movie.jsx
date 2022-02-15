870414d003f37b0db74c67b8d39eacccf4f79911
Guillem París Samper <38705449+GuillemParisSamper@users.noreply.github.com>
2022-02-15 02:08:41 +0100

    adjusted the scss displayed in the div elements Movies

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -4,7 +4,7 @@ import './Movie.scss'
 const Movie = ({movie}) => {
     return <div className="movie">
         <img src={"https://image.tmdb.org/t/p/w185"+movie.poster_path} alt={movie.title} />
-        <div>{movie.vote_average}</div>
+        <div><p>{movie.vote_average}</p></div>
         <h4>{movie.title}</h4>
         <p>{movie.release_date}</p>
     </div>
